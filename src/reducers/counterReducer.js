9c522c8805e707fbf9e87693f60eaf905811f844
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-03 12:32:06 -0600

    commit tree 6aef5db2214268fe240f3aa48e58e48637977be8

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,15 +1,14 @@
-export default function counterReducer(
-	state = {
-		clicks: 0
-	},
-	action
-) {
+export default function counterReducer(state = {clicks: 0}, action) {
+	console.log(action)
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.clicks', state.clicks)
+			console.log('Updating state.clicks to', state.clicks + 1)
 			return {
 				clicks: state.clicks + 1
 			}
 		default:
+			console.log('Initial state.clicks:', state.clicks)
 			return state;
 	}
 }
